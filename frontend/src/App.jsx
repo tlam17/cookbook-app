322a2335cbbf8e98a769275ed0e9b8cc29318f9d
tlam17 <94070835+tlam17@users.noreply.github.com>
2024-11-21 18:21:36 -0700

    Recreated frontend using Vite+React to use ShadCN

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+  return (
+    <>
+    <div className="px-20">
+      <Button>Click me</Button>
+    </div>
+    </>
+  )
+}
+
+export default App
